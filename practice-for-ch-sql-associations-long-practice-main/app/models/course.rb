274f27228c9b6a0bbb703068f3ae6585728981e1
yuris <yls4@cornell.edu>
2023-09-21 13:55:47 -0400

    completed user and enrollment associations

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
 end
