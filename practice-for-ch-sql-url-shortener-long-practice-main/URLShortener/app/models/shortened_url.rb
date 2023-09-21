0c31a025a3168ab449d976eb9ebdc9733021dec6
yuris <yls4@cornell.edu>
2023-09-21 16:41:39 -0400

    finished phase2

--- practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/app/models/shortened_url.rb
+++ practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/app/models/shortened_url.rb
@@ -1,3 +1,29 @@
 class ShortenedUrl < ApplicationRecord 
+    after_initialize :generate_short_url
+
     validates :short_url, presence: true, uniqueness: true 
+
+    def self.random_code
+        exists = false
+        until exists
+            random = SecureRandom.urlsafe_base64
+            if !self.exists?(short_url: random)
+                return random 
+                exists true 
+            end
+        end 
+    end
+
+    belongs_to(
+        :submitter,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
+    )
+
+    private
+
+    def generate_short_url
+        self.short_url = ShortenedUrl.random_code
+    end
 end 
--- practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/app/models/user.rb
+++ practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/app/models/user.rb
@@ -9,4 +9,11 @@
 #
 class User < ApplicationRecord
     validates :email, presence: true
+
+    has_many(
+        :submitted_urls,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :ShortenedUrl
+    )
 end
